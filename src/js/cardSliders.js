07c3a56e9a3845258d65a58088355a2e05dc6440
Artem Safin <artemsafin.webdev@gmail.com>
2021-01-18 11:51:01 +0300

    Карточки продуктов

---
+++ src/js/cardSliders.js
@@ -0,0 +1,77 @@
+import { Swiper, Pagination, EffectFade } from 'swiper';
+
+Swiper.use([Pagination, EffectFade]);
+
+function initializeCardSlider(card) {
+    const container = card.querySelector('.swiper-container');
+    const DEBUG = false;
+    if (!container) {
+        console.error('No swiper container in card', card);
+        return;
+    }
+
+    let trackingCursor = false;
+    let currentSlideIndex;
+
+    const instance = new Swiper(container, {
+        watchOverflow: true,
+        effect: 'fade',
+        speed: 600,
+        longSwipesRatio: 0.25,
+        fadeEffect: {
+            crossFade: true
+        },
+        pagination: {
+            el: card.querySelector('.card__gallery-pagination'),
+            type: 'bullets',
+            clickable: true
+        }
+    })
+
+    const slidesCount = instance.slides.length;
+
+    container.addEventListener('mouseenter', ()=> {
+        trackingCursor = true;
+    })
+    container.addEventListener('mouseleave', () => {
+        trackingCursor = false;
+        instance.slideTo(0);
+    })
+
+    container.addEventListener('mousemove', e => {
+        if (!trackingCursor) return;
+        e.stopPropagation();
+      
+        const rect = e.currentTarget.getBoundingClientRect();
+        const offsetX = parseInt(e.clientX - rect.left, 10);
+        
+        const width = e.currentTarget.offsetWidth;
+
+        const progress = Math.ceil(offsetX / width * slidesCount);
+        let activeSlideIndex = progress - 1;
+
+        if (DEBUG) {
+            console.log({
+                activeSlideIndex,
+                progress,
+                offsetX
+            })
+        }
+        
+        
+       
+        if (activeSlideIndex !== currentSlideIndex) {
+            instance.slideTo(activeSlideIndex);
+        }
+    });
+}
+
+window.initializeCardSlider = initializeCardSlider;
+
+export function cardSliders() {
+    const elements = Array.from(document.querySelectorAll('.card'))
+
+    elements.forEach(card => {
+        initializeCardSlider(card);
+    })
+}
--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,13 @@ import polyfills from './polyfills';
 import './lazyload';
 import detectTouch from './detectTouch';
 import homeSlider from './homeSlider';
+import { cardSliders } from './cardSliders';
 
 document.addEventListener('DOMContentLoaded', function() {
     polyfills();
     detectTouch();
     homeSlider();
+    cardSliders();
 });
 
 window.addEventListener('load', function() {
